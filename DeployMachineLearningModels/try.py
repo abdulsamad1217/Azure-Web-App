8b5ffa3de2224390887a00271e8363a2e0582d10
Abdul Samad <61932669+abdulsamad1217@users.noreply.github.com>
2022-05-20 18:25:04 +0530

    Update try.py

--- DeployMachineLearningModels/try.py
+++ DeployMachineLearningModels/try.py
@@ -74,7 +74,7 @@ allowSelfSignedHttps(True) # this line is needed if you use self-signed certific
 body = str.encode(json.dumps(data))
 
 url = 'http://20.221.6.249:80/api/v1/service/aks-service/score'
-api_key = os.environ.get('CCProject')    # Replace this with the API key for the web service
+api_key = 'mwmzRYDjqHMdTIO5H1xoBIVA24nVy4FZ'    # Replace this with the API key for the web service
 headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
 
 req = urllib.request.Request(url, body, headers)
